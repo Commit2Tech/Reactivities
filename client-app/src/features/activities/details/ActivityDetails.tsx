ddbc2f50d658692e5f54e983c7f94d70cf4e9e64
Shivendra Singh <1579@xceedance.com>
2021-08-07 21:05:05 +0530

    End of section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button, Card, Image } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from '../../../app/stores/store';
 
-interface Props{
-    activity: Activity;
-    cancelActivity: () => void;
-    openForm: (id : string) => void;
-}
-export default function ActivityDetails({activity, cancelActivity, openForm }: Props) {
+export default function ActivityDetails() {
+    const {activityStore} =  useStore();
+    const {selectedActivity: activity, openForm, cancelActivity} = activityStore;
+
+    if (!activity) return <LoadingComponent/>; 
+
     return (
         <Card fluid>
             <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
